efdefed3e25635004024cf16d7749fa6cbb21f67
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-11-06 18:14:27 -0800

    Initial Commit

---
+++ Swiftuidemoapp/SwiftuidemoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuidemoappApp.swift
+//  Swiftuidemoapp
+//
+//  Created by Farad Niftaliyev on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuidemoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
