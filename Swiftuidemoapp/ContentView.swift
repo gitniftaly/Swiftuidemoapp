7f34aa8157a75ec2a0f4dad745b385d44c73bd8e
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-11-07 20:37:10 -0800

    if else condition with constant varabels added

--- Swiftuidemoapp/ContentView.swift
+++ Swiftuidemoapp/ContentView.swift
@@ -8,29 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var message = "Daldan daldan"
+//    @State var message = "Daldan daldan"
+    @State private var message = ""
+    @State private var imgString = ""
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: "swift")
+            Image(systemName: imgString)
                 .resizable()
                 .imageScale(.large)
                 .foregroundColor(.orange)
                 .scaledToFit()
                 .frame(width: 200, height: 200)
-                
+            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.ultraLight)
             Spacer()
-            HStack{
-                Button("Awesome!") {
-                    message = "Very sweet dall!"
+            //            HStack{
+            //                Button("Awesome!") {
+            //                    message = "Very sweet dall!"
+            //                }
+            //                Button("Great!") {
+            //                    message = "This is great!"
+            //                }
+            //            }
+            Button("Press me!") {
+                let message1 = "You are Awesome"
+                let message2 = "You are Great"
+                let imgString1 = "hand.thumbsup"
+                let imgString2 = "sun.max.fill"
+                if message == message1{
+                    message = message2
+                    imgString = imgString1
+                }else {
+                    message = message1
+                    imgString = imgString2
                 }
-                Button("Great!") {
-                    message = "This is great!"
-                }
+              
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
