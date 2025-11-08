8367dd32da45e1e7a0b9bbd4e15ef770680c8317
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-11-08 12:22:32 -0800

    working with images and clip modifire and shadow

--- Swiftuidemoapp/ContentView.swift
+++ Swiftuidemoapp/ContentView.swift
@@ -10,17 +10,19 @@ import SwiftUI
 struct ContentView: View {
 //    @State var message = "Daldan daldan"
     @State private var message = ""
-    @State private var imgString = ""
+    @State private var imgName = ""
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: imgString)
+            Image(imgName)
                 .resizable()
                 .imageScale(.large)
                 .foregroundColor(.orange)
                 .scaledToFit()
-                .frame(width: 200, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+//                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
@@ -37,15 +39,18 @@ struct ContentView: View {
             Button("Press me!") {
                 let message1 = "You are Awesome"
                 let message2 = "You are Great"
-                let imgString1 = "hand.thumbsup"
-                let imgString2 = "sun.max.fill"
-                if message == message1{
-                    message = message2
-                    imgString = imgString1
-                }else {
-                    message = message1
-                    imgString = imgString2
-                }
+                message = (message == message1 ? message2 : message1)
+                imgName = (imgName == "image0" ? "image1" : "image0")
+//                let imgString1 = "hand.thumbsup"
+//                let imgString2 = "sun.max.fill"
+                
+//                if message == message1{
+//                    message = message2
+//                    imgName = imgString1
+//                }else {
+//                    message = message1
+//                    imgName = imgString2
+//                }
               
             }
             .buttonStyle(.borderedProminent)
