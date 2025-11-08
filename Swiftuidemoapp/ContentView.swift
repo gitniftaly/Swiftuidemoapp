ec7eaab07380793fe22e5bf0dc2779427c49c31d
Fakhraddin Niftaliyev <v-fniftaliye@microsoft.com>
2025-11-08 14:04:43 -0800

    looked into types of data used by swiftui string, boolean, number and double

--- Swiftuidemoapp/ContentView.swift
+++ Swiftuidemoapp/ContentView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State var message = "Daldan daldan"
+    //    @State var message = "Daldan daldan"
     @State private var message = ""
     @State private var imgName = ""
+    @State private var imgNumber = 0
+    
     var body: some View {
         
         VStack {
@@ -22,13 +24,13 @@ struct ContentView: View {
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-//                .frame(width: 200, height: 200)
+            //                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.ultraLight)
             Spacer()
-            //            HStack{
+            //            {
             //                Button("Awesome!") {
             //                    message = "Very sweet dall!"
             //                }
@@ -40,18 +42,24 @@ struct ContentView: View {
                 let message1 = "You are Awesome"
                 let message2 = "You are Great"
                 message = (message == message1 ? message2 : message1)
-                imgName = (imgName == "image0" ? "image1" : "image0")
-//                let imgString1 = "hand.thumbsup"
-//                let imgString2 = "sun.max.fill"
+                imgNumber += 1
+                if imgNumber > 9 {
+                    imgNumber = 0
+                    
+                }
+                imgName = "image\(imgNumber)"
+                //                print(imgNumber)
+                //                let imgString1 = "hand.thumbsup"
+                //                let imgString2 = "sun.max.fill"
                 
-//                if message == message1{
-//                    message = message2
-//                    imgName = imgString1
-//                }else {
-//                    message = message1
-//                    imgName = imgString2
-//                }
-              
+                //                if message == message1{
+                //                    message = message2
+                //                    imgName = imgString1
+                //                }else {
+                //                    message = message1
+                //                    imgName = imgString2
+                //                }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
